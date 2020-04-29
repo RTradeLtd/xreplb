bc25cf1f9a7a8dcde5b15d58427b4b43a383d8e1
postables <postables@rtradetechnologies.com>
2020-04-28 22:52:37 -0700

    cleanup repo and start work to bench temporalx and go-ipfs v0.5.0

---
+++ replication/temporalx/flow_2.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+cat << EOF >  replication.yml
+topic: flow 2
+author: 12D3KooWGrjfQVFPBoPXGBjwLpDtyQZ9iFCD55BjVxndH9YKHw2n
+version: 1
+redundancy: 3
+cids: []
+servers:
+- id: 12D3KooWGrjfQVFPBoPXGBjwLpDtyQZ9iFCD55BjVxndH9YKHw2n
+  grpcport: 9094
+  addresses:
+  - /ip4/127.0.0.1/tcp/4005
+- id: 12D3KooWC2HDv4NtkrEgZyDaHCbE7SzwFciJihCCdaoacNtEnTi1
+  grpcport: 9095
+  addresses:
+  - /ip4/127.0.0.1/tcp/4006
+- id: 12D3KooWEeHhDv1ygsaKDNX6SFMrTSxyN4KXGkzQJLH2YqYUjUsP
+  grpcport: 9096
+  addresses:
+  - /ip4/127.0.0.1/tcp/4007
+authorprivatekey: ""
+clientprivatekey: ""
+EOF
